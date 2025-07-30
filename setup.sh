eaed0d4c29c895a59d7a5bd11332d2648354c815
Kilynn Weber <cheesy.email@gmail.com>
2025-07-30 10:48:15 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -16,7 +16,7 @@ python -c "import nltk; nltk.download('punkt');nltk.download('stopwords');nltk.d
 
 python -m spacy download en_core_web_sm
 echo "python src/pipeline.py --input_dir path/to/docs --output_dir output/"
-bash export SSL_CERT_FILE=$(python -m certifi)
+export SSL_CERT_FILE=$(python -m certifi)
 
 # Print confirmation
 echo "Virtual environment activated and dependencies installed."
