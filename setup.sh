e90d8359fc472c15d51f05c2234bd77e9c0f884e
Kilynn Weber <cheesy.email@gmail.com>
2025-07-30 10:51:38 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,6 +8,7 @@ mkdir -p examples
 mkdir -p output
 # Activate the virtual environment
 source venv/bin/activate
+export SSL_CERT_FILE=$(python -m certifi)
 
 # Install dependencies
 pip install -r requirements.txt --trusted-host pypi.org --trusted-host files.pythonhosted.org --quiet
@@ -16,7 +17,6 @@ python -c "import nltk; nltk.download('punkt');nltk.download('stopwords');nltk.d
 
 python -m spacy download en_core_web_sm
 echo "python src/pipeline.py --input_dir path/to/docs --output_dir output/"
-export SSL_CERT_FILE=$(python -m certifi)
 
 # Print confirmation
 echo "Virtual environment activated and dependencies installed."
