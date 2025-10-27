34a4a5217054eb850f86ec8f6435d18920eaf34b
Kilynn Weber <cheesy.email@gmail.com>
2025-10-27 10:33:10 -0500

    Modify setup.sh to upgrade PIP and use en_core_web_lg
    Updated setup script to upgrade PIP and change spaCy model.

--- setup.sh
+++ setup.sh
@@ -6,17 +6,19 @@ if [ ! -d "venv" ]; then
 fi
 mkdir -p examples
 mkdir -p output
+
 # Activate the virtual environment
 source venv/bin/activate
-export SSL_CERT_FILE=$(python -m certifi)
-
+# Upgrade PIP
+python -m pip install --upgrade pip --trusted-host pypi.org --trusted-host files.pythonhosted.org
 # Install dependencies
 pip install -r requirements.txt --trusted-host pypi.org --trusted-host files.pythonhosted.org --quiet
+# Set the Environment
+export SSL_CERT_FILE=$(python -m certifi)
 
 python -c "import nltk; nltk.download('punkt');nltk.download('stopwords');nltk.download('punkt_tab')"
 
-python -m spacy download en_core_web_sm
-echo "python src/pipeline.py --input_dir path/to/docs --output_dir output/"
+python -m spacy download en_core_web_lg
 
 # Print confirmation
 echo "Virtual environment activated and dependencies installed."
